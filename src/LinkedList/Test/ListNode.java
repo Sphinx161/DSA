c732c62de4cc904685eb7a6375888e6a614f404e
sphinx <1610shibani@gmail.com>
2020-09-09 20:58:58 +0530

    Initial commit

---
+++ src/LinkedList/Test/ListNode.java
@@ -0,0 +1,27 @@
+package LinkedList.Test;
+
+public class ListNode {
+    private int data;
+    private ListNode next;
+
+    ListNode(int data){
+        this.data = data;
+        next = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+}
